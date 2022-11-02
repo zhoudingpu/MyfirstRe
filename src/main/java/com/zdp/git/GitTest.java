873316d05b76cdd78f2223b0e5dc3d7569366d72
itcast <hello@itcast.cn>
2022-11-02 18:50:21 +0800

    hot-fix修改

--- src/main/java/com/zdp/git/GitTest.java
+++ src/main/java/com/zdp/git/GitTest.java
@@ -10,5 +10,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("first-update");
         System.out.println("second-update");
+        System.out.println("fourth-update");
+
     }
 }
