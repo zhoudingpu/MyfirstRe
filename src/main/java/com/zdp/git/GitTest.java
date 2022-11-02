bea12abc5dbe5df3c73ed5e7dbba3dc644995cba
itcast <hello@itcast.cn>
2022-11-02 18:37:49 +0800

    提交了两个修改

---
+++ src/main/java/com/zdp/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.zdp.git;
+
+/**
+ * @Classname GitTest
+ * @Description
+ * @Date 2022/11/2
+ * @Author MAMBA
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("first-update");
+        System.out.println("second-update");
+    }
+}
